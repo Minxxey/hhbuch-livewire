4262ae37f3108e9644307047f73cd97dd5e04391
Minxxey <lina.staudt@googlemail.com>
2025-07-17 21:19:48 +0200

    feat(haus-15): added tag model, db, seeder, factory, migration, enum

---
+++ app/Enum/TagEnum.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Enum;
+
+enum TagEnum: string
+{
+    case SHOPPING = 'einkaufen';
+    case DINING_OUT = 'essen gehen';
+    case TRAVEL = 'reise';
+    case EVENTS = 'events';
+    case MEDICINE = 'medizin';
+    case OTHER = 'sonstiges';
+
+    public function color() {
+        return match($this)
+        {
+            self::SHOPPING => '#88AA99',
+            self::DINING_OUT => '#6B9080',
+            self::TRAVEL => '#A4C3B2',
+            self::EVENTS => '#CCE3DE',
+            self::MEDICINE => '#EAF4F4',
+            self::OTHER => '#F6FFF8',
+        };
+    }
+
+}
---
+++ database/seeders/TagSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enum\TagEnum;
+use App\Models\Tag;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        foreach (TagEnum::cases() as $tag) {
+            Tag::factory(
+                ['name' => $tag->name],
+                ['color' => $tag->color()]
+            )->create();
+        }
+    }
+}
