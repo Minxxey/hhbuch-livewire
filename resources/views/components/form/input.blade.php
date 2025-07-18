2a3c05e4a9fa0a7dfc77e1296e084ecf2a0376e9
Minxxey <lina.staudt@googlemail.com>
2025-07-18 13:07:19 +0200

    feat(haus-8): added missing attributes

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -5,5 +5,7 @@
 ])
 <div class="input flex flex-col mb-1">
     <label for="{{$name}}" class="text-text text-lg mb-[10px]">{{$labelText}}</label>
-    <input type="{{$type}}" class="border-text border-1 py-[5px] px-[15px] w-full outline-text ring-0" id="{{$name}}">
+    <input type="{{$type}}" id="{{$name}}" name="{{$name}}" {{ $attributes->merge([
+            'class' => 'border-text border-1 py-[5px] px-[15px] w-full outline-text ring-0 text-text'
+        ]) }}>
 </div>
--- resources/views/components/form/select.blade.php
+++ resources/views/components/form/select.blade.php
@@ -6,7 +6,8 @@
 ])
 <div class="flex flex-col mb-1 text-text text-lg">
     <label for="{{ $name }}" class="text-text text-lg mb-[10px]">{{ $labelText }}</label>
-    <select name="{{ $name }}" id="{{ $name }}" class="border-text border-1 py-[5px] px-[5px] outline-text ring-0">
+    <select name="{{ $name }}" id="{{ $name }}"
+            class="border-text border-1 py-[5px] px-[5px] outline-text ring-0" {{ $attributes->whereStartsWith('wire:model') }}>
         <option value="">Bitte wählen</option>
         @foreach($arrayData as $key => $value)
             <option value="{{ $key }}" class="bg-background">{{ $value }}</option>
