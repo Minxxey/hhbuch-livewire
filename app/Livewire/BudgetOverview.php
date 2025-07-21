c0d5a2c6aef610d6cbc262dc6da940dd9562c14a
Minxxey <lina.staudt@googlemail.com>
2025-07-21 17:29:48 +0200

    feat(haus-12): added all budget overview info

--- app/Livewire/BudgetOverview.php
+++ app/Livewire/BudgetOverview.php
@@ -2,12 +2,21 @@
 
 namespace App\Livewire;
 
+use App\Models\Entry;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class BudgetOverview extends Component
 {
+    #[Validate('required|string')]
+    public string $month = '';
+    public array $months = [];
 
-    protected array $months = [];
+    public float $expensesTimo = 0;
+    public float $expensesLina = 0;
+    public float $totalExpenses = 0;
+    public float $sharedExpenses = 0;
+
 
     public function mount()
     {
@@ -31,5 +40,20 @@ class BudgetOverview extends Component
     public function render()
     {
         return view('livewire.budget-overview');
+    }
+
+    public function loadBudgetData()
+    {
+        $entriesLina = Entry::where('user_id', 1)
+            ->where('month', $this->month)
+            ->get();
+        $entriesTimo = Entry::where('user_id', 2)
+            ->where('month', $this->month)->get();
+
+        $this->expensesLina = $entriesLina->sum('price');
+        $this->expensesTimo = $entriesTimo->sum('price');
+        $this->totalExpenses = $this->expensesLina + $this->expensesTimo;
+        $this->sharedExpenses = (float) $this->totalExpenses / 2;
+
     }
 }
--- resources/views/livewire/budget-overview.blade.php
+++ resources/views/livewire/budget-overview.blade.php
@@ -1,3 +1,12 @@
-<div class="budget-overview container">
-   <h1 class="text-text">hi</h1>
+<div class="budget-overview container block max-w-[90%] mx-auto md:mx-automd:max-w-[800px] my-2" x-data="{ month: @entangle('month') }">
+    <h1 class="text-text text-center text-lg">Wähle einen Monat, um die entsprechende Auswertung anzuzeigen</h1>
+   <x-form.select name="month" :arrayData="$months" wire:model="month" wire:change="loadBudgetData">
+   </x-form.select>
+
+    <div class="budget-overview-calc border border-text rounded-md px-[10px] py-1 text-text"  x-show="month" x-transition>
+        <h2 class="text-lg text-left">Ausgaben insgesamt: </h2>
+        <p>Timo: {{$expensesTimo}}</p>
+        <p>Lina: {{$expensesLina}}</p>
+        <p>= {{$totalExpenses}} / 2 = {{$sharedExpenses}}</p>
+    </div>
 </div>
